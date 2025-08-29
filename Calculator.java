c955513e4a217272a4739fb5c01cbd3b5c460eed
Amitkumar Mourya <amitinfo1309@gmail.com>
2025-08-29 14:46:39 +0530

    update calaculator.java

--- Calculator.java
+++ Calculator.java
@@ -17,7 +17,8 @@ public class Calculator {
         int sum = calc.add(10, 5);
         int difference = calc.subtract(10, 5);
 
-        System.out.println("Addition: " + sum);        // Output: Addition: 15
+        System.out.println("Addition: " + sum);
+        System.out.println("New Git psuh");        // Output: Addition: 15
         System.out.println("Subtraction: " + difference); // Output: Subtraction: 5
     }
 }
